9e1760985e78824dad15b67208607ed57a2cf6b4
FEhyoeun <khyoeun92@gmail.com>
2022-07-18 22:20:14 +0900

    nuxt의 데이터 호출 방식과 API 연동

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,4 +34,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  // server setUp
+  server: {
+    port: 8000,
+  }
 }
--- pages/mainComponent.vue
+++ pages/mainComponent.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
     <h1>main pages</h1>
+    <ProductList />
   </div>
 </template>
 
 <script>
+
+import ProductList from "@/components/ProductList.vue";
+
 export default {
-  name: "mainComponent"
+  name: "MainComponent",
+  components: {ProductList},
+  // async asyncData() {
+  //   const response = await axios.get('http://localhost:3000/products')
+  //   const products = response.data
+  //   return {products} // 객체 축약 문법. user: user임. asyncData에서는 이 return에 있는 값을 this.xxx 할 필요 없이 data처럼 사용할 수 있음.
+  //   // 여기서 this를 사용할 수 없는 건 asyncData는 페이지가 그려지기 전에(컴포넌트가 생성되기 이전에) 호출되는 메소드이므로 this로 접근할 수 없음
+  // },
+
 }
 </script>
 
